8df55206d887592e76f1133a36ffb9a4ecbc357f
ChristianVillacastin <cvbchristian@gmail.com>
2021-04-06 09:31:09 +0100

    Aplicacion PWA de pelíclas. Instalable mediante un botón. Permite ver, modificar y eliminar una lista de películas

---
+++ public/files/install.js
@@ -0,0 +1,40 @@
+'use strict';
+
+let deferredInstallPrompt = null;
+const installButton = document.getElementById('butInstall');
+installButton.addEventListener('click', installPWA);
+
+window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+
+function saveBeforeInstallPromptEvent(evt) {
+
+    deferredInstallPrompt = evt;
+    installButton.removeAttribute('hidden');
+
+}
+
+function installPWA(evt) {
+
+    deferredInstallPrompt.prompt();
+
+    evt.srcElement.setAttribute('hidden', true);
+    defferredInstallPrompt.userChoice.then((choice) => {
+
+        if (choice.outcome === "accepted") {
+
+            console.log("Accepted")
+        } else {
+
+            console.log("Not accepted")
+        }
+
+        //deferredInstallPrompt = null;
+    })
+}
+
+window.addEventListener('appinstalled', logAppInstalled);
+
+function logAppInstalled(evt) {
+
+    console.log("Pelis instalada app");
+}
